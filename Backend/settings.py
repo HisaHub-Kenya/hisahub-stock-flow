8dfe63f2132e2c76455ee8e170a1a7ad8e1f4eaf
HisaHub <okwaraian@gmail.com>
2025-09-20 14:30:02 +0300

    Fix: define BASE_DIR for STATIC_ROOT

--- Backend/settings.py
+++ Backend/settings.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
